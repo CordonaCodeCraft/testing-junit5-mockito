f5ea42acef02c970890448adb62aba8a545327d0
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-03-20 12:35:03 +0200

    Added test to demonstrate capture arguments.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -5,9 +5,14 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -28,6 +33,37 @@ class OwnerControllerTest {
     @Mock
     BindingResult bindingResult;
 
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Test
+    void processFindFormWilcardString() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> owners = new ArrayList<>();
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(owners);
+
+        //when
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+
+        //then
+        assertThat("%Buck%").isEqualToIgnoringCase(captor.getValue());
+    }
+
+    @Test
+    void processFindFormWildcardStringAnnotation() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> owners = new ArrayList<>();
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(owners);
+
+        //when
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+
+        //then
+        assertThat("%Buck%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+    }
 
     @Test
     void processCreationFormHasErrors() {
